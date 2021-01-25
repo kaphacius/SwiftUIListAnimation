08d83af2c9dad4da60be2651fd2458b76d96eed9
Yurii Zadoianchuk <yurii@styleshoots.com>
2021-01-25 13:51:20 +0100

    Initial Commit

---
+++ swiftui-list-animation/swiftui_list_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_list_animationApp.swift
+//  swiftui-list-animation
+//
+//  Created by Yurii Zadoianchuk on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_list_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
